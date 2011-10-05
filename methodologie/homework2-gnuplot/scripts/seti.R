d451f05f350a85d00c747a71712a230341d3f395
Rémy Léone <remy.leone@gmail.com>
2011-10-05 10:02:04 +0200

    Corrections

--- methodologie/homework2-gnuplot/scripts/seti.R
+++ methodologie/homework2-gnuplot/scripts/seti.R
@@ -5,7 +5,13 @@ d<-read.table("seti-output.txt",sep=",")
 colnames(d) <-  c("x","nrml")
 
 pdf(file='../img/part2.pdf')
-hist( d$nrml, nclass=30,prob=TRUE,main="Uptime time",ylim = c(0,1),xlab="Normalized time")
+hist( d$nrml,
+    nclass=30,
+    prob=TRUE,
+    main="Uptime time",
+    ylim = c(0,1),
+    xlab="Normalized time")
 lines(density(d$nrml[!is.na(d$nrml)]))
 
 dev.off()
+
