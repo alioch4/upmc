80c11da96412acfb658e048f0833b170ced1625d
Rémy Léone <remy.leone@gmail.com>
2011-11-22 10:53:20 +0100

    Ca marche bien

--- ggt/graphe.py
+++ ggt/graphe.py
@@ -8,23 +8,27 @@ class Graph():
     n = 0 # Nombre de noeuds
     m = 0 # Nombre d'arêtes
     nodes = dict()
+    degree = dict()
+
+    def __init__(self, value):
+        f = open( value, "r" )
+        reader = csv.reader(f, delimiter=" ")
+
+        for i in reader:
+            self.AddNode(i[0], i[1])
+            self.AddNode(i[1], i[0])
+
+    def __str__(self):
+        return str(self.nodes)
 
     def AddNode(self, node1, node2):
         if( self.nodes.has_key( node1 ) ):
             self.nodes[ node1 ].add( node2 )
-            self.nodes[ node1 ]["degree"] += 1
+            self.degree[ node1 ] += 1
         else:
-            self.nodes[ node1 ] = dict()
-            self.nodes[ node1 ]["degree"] = 1
-
-
-    def CreateGraph(self, *args, **options):
-        f = open( args[1], "r" )
-        reader = csv.reader(f, delimiter=" ")
-
-        for i in reader:
-            self.AddNode(self, i[0], i[1])
-            self.AddNode(self, i[1], i[0])
+            self.nodes[ node1 ] = set()
+            self.nodes[ node1 ].add( node2 )
+            self.degree[ node1 ] = 1
 
     def DegreeGraph(self):
         res = dict()
@@ -34,7 +38,7 @@ class Graph():
 
     # Nombre de noeuds dans le graphe
 
-    def CardGraph( self ):
+    def CardGraph():
         return self.n
 
     # Nombre de sommets de degré 0
@@ -50,13 +54,14 @@ class Graph():
 
     def DistDegreeGraph( self ):
         res = dict()
-        for i in self.nodes:
+        for i in self.degree:
             if( res.has_key( i["degree"] ) ):
-                res[i["degree"]] +=
+                res[i["degree"]] += 1
             else:
                 res[ i["degree"] ] = 1
         return res
 
 
-g = Graph()
-g.CreateGraph("graphe.txt")
+g = Graph("graphe.txt")
+print g
+print g.degree
