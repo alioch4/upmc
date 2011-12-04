1f6e2c23834a2639c824035dd223a8ceda2451e0
Rémy Léone <remy.leone@gmail.com>
2011-12-04 16:21:06 +0100

    On modifie le fichier de run

--- ggt/run.py
+++ ggt/run.py
@@ -2,18 +2,17 @@
 # -*- coding:utf-8 -*-
 
 import os, sys
+
 cmd_folder = os.path.dirname(os.path.abspath(__file__))
 if cmd_folder not in sys.path:
     sys.path.insert(0, cmd_folder)
-print sys.path
 
-from graphe import *
+from graph import *
+
 
 n_test = 0
 
 original = Graph("data/flikr-test")
 sample   = Graph("data/flikr-test")
-EraseLink(sample)
-
-test_lien( original, sample, 0, 7)
+print sample.EraseLink()
 
