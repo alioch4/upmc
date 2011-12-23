c65d5431d8ce4a6d34545c8d769c9166347c2763
Rémy Léone <remy.leone@gmail.com>
2011-12-23 21:06:11 +0100

    Corrections de quelques erreurs

--- ggt/community.py
+++ ggt/community.py
@@ -4,15 +4,16 @@
 import os
 import csv
 
-def distribution( address ):
-    f = open( address, "r")
+
+def distribution(address):
+    f = open(address, "r")
     reader = csv.reader(f, delimiter=" ")
 
     res = dict()
     pool = dict()
 
     for i in reader:
-        if( pool.has_key( int(i[1]) )):
+        if int(i[1]) in pool:
             pool[int(i[1])] += 1
         else:
             pool[int(i[1])] = 1
@@ -31,16 +32,19 @@ def conversion():
     os.system("community/convert -i data/flikr-test -o data/flikr-test.bin")
     os.system("community/convert -i data/flikr -o data/flikr.bin")
 
+
 def community():
     os.system("community/community data/flikr-test.bin -l -1 -q 0.0001 >\
     data/flikr-test.tree")
     os.system("community/community data/flikr.bin -l -1 -q 0.0001 >\
     data/flikr.tree")
 
+
 def hierarchy():
     os.system("community/hierarchy -l 3 data/flikr-test.tree >\
             data/flikr-test.data")
     os.system("community/hierarchy -l 2 data/flikr.tree > data/flikr.data")
+
 
 conversion()
 community()
@@ -48,16 +52,16 @@ hierarchy()
 
 print "Distribution de flikr-test"
 res = distribution("data/flikr-test.data")
-output = open('data/dist-flikr-test.plot',"w")
+output = open('data/dist-flikr-test.plot', "w")
 for i in res:
-    output.write("%s %s\n" % (i,res[i]))
+    output.write("%s %s\n" % (i, res[i]))
 output.close()
 
 pass
 
 print "Distribution de flikr"
 res = distribution("data/flikr.data")
-output = open('data/dist-flikr.plot',"w")
+output = open('data/dist-flikr.plot', "w")
 for i in res:
-    output.write("%s %s\n" % (i,res[i]))
+    output.write("%s %s\n" % (i, res[i]))
 output.close()
